0b321c6e8b9bcf8706d5a46c37fa6e85f686c4ea
ManaPHP Mark <manaphp@qq.com>
2019-06-29 14:30:34 +0800

    Modified   .env.example Modified   README.md Modified   app/Views/Errors/500.phtml Modified   config/app.php Modified   public/index.php

--- config/app.php
+++ config/app.php
@@ -7,20 +7,15 @@ return [
     'version' => '1.1.1',
     'timezone' => 'PRC',
     'master_key' => env('MASTER_KEY'),
-    'compatible_globals' => false,
-    'servers' => [
-        'http' => ['worker_num' => 1, 'max_request' => 1000000, 'dispatch_mode' => 1]
-    ],
     'params' => [],
     'aliases' => [],
     'components' => [
-        'db' => [env('DB_URL')],
-        'redis' => [env('REDIS_URL')],
-        'mongodb' => [env('MONGODB_URL')],
+        '!httpServer' => ['worker_num' => 4, 'max_request' => 1000000, 'dispatch_mode' => 1, 'enable_static_handler' => env('APP_DEBUG', false)],
+        'db' => env('DB_URL'),
+        'redis' => env('REDIS_URL'),
         'logger' => ['level' => env('LOGGER_LEVEL', 'info')],
     ],
     'services' => [],
-    'listeners' => [],
     'plugins' => [
         //'debugger',
         //'fiddler',
